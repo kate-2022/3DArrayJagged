942ff1474e253035553d071247f6bf261d9d9af6
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2022-10-31 11:25:01 +0100

    should work :)

---
+++ src/ThreeD.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ThreeD {
+
+	public static void main(String[] args) {
+	
+		    int size=0;
+		    int topicNumber=1;
+		    Scanner scan = new Scanner(System.in);
+		    
+			System.out.println("PLease enter number of topics per semester:  ");
+			size=scan.nextInt();
+		    
+		    int [] [] [] students = new int [3] [2] [size];
+
+		    for (int i=0; i<students.length; i++)
+		    {
+		    	System.out.println( "Please insert student class no.:   ");
+    			scan.nextInt();
+    			
+		    	for (int j=0; j<students[i].length; j++)
+		    	{
+		    		System.out.println( "Please insert students id:  ");
+	    			scan.nextInt();
+	    			
+		    		for (int k=0; k<students[i][j].length; k++)
+		    		{
+		    			System.out.println("PLease enter students mark for topic number " + topicNumber+ " : " );
+		    			scan.nextInt();
+	                    if (topicNumber<=size)
+		    			topicNumber++;
+
+		    		}
+		    	}
+		    	
+		    }
+		        		
+	
+	}
+
+}
